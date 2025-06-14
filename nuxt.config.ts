1a104cae155af184bda87ec88ba6879f8d8bd112
Cristopher Guzmán <guzmncristopher@gmail.com>
2025-06-14 07:19:00 -0500

    :tada: initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+import tailwindcss from '@tailwindcss/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: false },
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  modules: [
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/fonts',
+    '@nuxt/eslint',
+  ],
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+})
