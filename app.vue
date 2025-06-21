6fca786266a7fc1987bb9d2f69202f09f5868ff3
Cristopher Guzmán <guzmncristopher@gmail.com>
2025-06-21 06:24:56 -0500

    :sparkles: add layout sidebar including sidebar component

--- app.vue
+++ app.vue
@@ -1,5 +1,30 @@
+<script lang="ts" setup>
+const showMenu = ref(false)
+// const showOrder = ref(false)
+
+function toggleMenu() {
+  showMenu.value = !showMenu.value
+}
+</script>
+
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="bg-[#262837] w-full min-h-screen">
+    <Sidebar :show-menu="showMenu" />
+    <!-- Menu mobile -->
+    <nav class="bg-[#1f1d2b] lg:hidden fixed w-full bottom-0 left-0 text-2xl text-gray-400 py-2 px-8 flex items-center justify-between rounded-tl-2xl rounded-tr-2xl">
+      <button class="p-2">
+        <Icon name="ri:user-3-line" />
+      </button>
+      <button class="p-2">
+        <Icon name="ri:add-line" />
+      </button>
+      <button class="p-2">
+        <Icon name="ri:pie-chart-line" />
+      </button>
+      <button class="p-2 text-white" @click="toggleMenu">
+        <Icon :name="showMenu ? 'ri:close-line' : 'ri:menu-3-line'" />
+      </button>
+    </nav>
+    <main class="lg:pl-28 grid grid-cols-1 lg:grid-cols-8 w-full " />
+  </div>
 </template>
---
+++ components/Sidebar.vue
@@ -0,0 +1,54 @@
+<script lang="ts" setup>
+defineProps<{ showMenu: boolean }>()
+</script>
+
+<template>
+  <div class="bg-[#1F1D2B] fixed lg:left-0 top-0 w-28 h-full flex flex-col justify-between py-6 rounded-tr-xl rounded-br-xl z-50 -left-full transition-all" :class="[showMenu ? 'left-0' : '-left-full']">
+    <div>
+      <h1 class="text-2xl text-gray-300 uppercase font-bold text-center my-5">
+        Logo
+      </h1>
+      <ul class="pl-4">
+        <li class="bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl">
+          <NuxtLink to="/" class="bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-white">
+            <Icon name="ri:home-6-line" class="text-white" />
+          </NuxtLink>
+        </li>
+        <li class="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+          <NuxtLink to="/" class="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white">
+            <Icon name="ri:percent-line" class="text-white" />
+          </NuxtLink>
+        </li>
+        <li class="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+          <NuxtLink to="/" class="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white">
+            <Icon name="ri:pie-chart-line" class="text-white" />
+          </NuxtLink>
+        </li>
+        <li class="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+          <NuxtLink to="/" class="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white">
+            <Icon name="ri:mail-line" class="text-white" />
+          </NuxtLink>
+        </li>
+        <li class="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+          <NuxtLink to="/" class="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white">
+            <Icon name="ri:notification-3-line" class="text-white" />
+          </NuxtLink>
+        </li>
+        <li class="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+          <NuxtLink to="/" class="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white">
+            <Icon name="ri:settings-4-line" class="text-white" />
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <ul class="pl-4">
+        <li class="hover:bg-[#262837] p-4 rounded-tl-xl rounded-bl-xl group transition-colors">
+          <NuxtLink to="/" class="group-hover:bg-[#ec7c6a] p-4 flex justify-center rounded-xl text-[#ec7c6a] group-hover:text-white">
+            <Icon name="ri:logout-circle-line" class="text-white" />
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
