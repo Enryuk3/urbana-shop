82cd626c39bc30b9d36f320db8dab99e1e578490
Cristopher Guzmán <guzmncristopher@gmail.com>
2025-06-27 21:26:14 -0500

    ADD: logo

---
+++ components/Logo.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+// No logic needed
+</script>
+
+<template>
+  <div class="w-14 h-14 inline-flex items-center justify-center bg-[#EB966A]/26 rounded-xl">
+    <Icon name="iconly:store" class="size-10" />
+  </div>
+</template>
