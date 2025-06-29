74775d662ba8d83276ba6852a86e889c4a5beecb
Cristopher Guzmán <guzmncristopher@gmail.com>
2025-06-29 18:18:57 -0500

    :sparkles: setup favicon app

--- app.vue
+++ app.vue
@@ -20,6 +20,13 @@ useHead({
   bodyAttrs: {
     class: 'bg-background',
   },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: '/favicon.svg',
+    },
+  ],
 })
 </script>
 
